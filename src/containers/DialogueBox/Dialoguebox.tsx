15d50ce64e69b0586fab63669eba63a16200a50a
Magaly Fernandez Garcia <fernandezgarciamagaly@gmail.com>
2024-10-23 13:14:14 +0200

    adding logic to chatBox

--- src/containers/DialogueBox/Dialoguebox.tsx
+++ src/containers/DialogueBox/Dialoguebox.tsx
@@ -1,17 +1,12 @@
 import "./dialoguebox.css";
 
-const DialogueBox = () => {
+const DialogueBox = ({ text = "" }: { text: string }) => {
 	return (
 		<>
-			<p className="dialogueBox">
-				Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet quod
-				dolor, aperiam tenetur nulla deleniti consectetur nam eaque veniam
-				deserunt, non quia quaerat odit officiis voluptatem fuga quidem rem
-				minima?
-			</p>
+			<p className="dialogueBox">{text === "" ? "oups" : text}</p>
 			<img
 				className="downArrow"
-				src="../../../public/down-long-solid (1).svg"
+				src="/public/down-long-solid (1).svg"
 				alt="down arrow"
 			/>
 		</>
--- src/containers/PagesCanvas/PagesCanvas.tsx
+++ src/containers/PagesCanvas/PagesCanvas.tsx
@@ -1,13 +1,25 @@
 import "./PagesCanvas.css";
 import DialogueBox from "../DialogueBox/Dialoguebox";
+import { useState } from "react";
 
 const PageCanvas = () => {
+	const arrayOfDialogue = [
+		"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet quod dolor, aperiam tenetur nulla deleniti consectetur nam eaque veniam deserunt, non quia quaerat odit officiis voluptatem fuga quidem rem minima?",
+		"J'ai réussi!",
+	];
+	const [index, setIndex] = useState(0);
+
+	const displayDialogue = () => {
+		setIndex((prevIndex) =>
+			prevIndex < arrayOfDialogue.length - 1 ? prevIndex + 1 : 0
+		);
+	};
 	return (
-		<main className="main">
+		<main className="main" onClick={() => displayDialogue()}>
 			<section className="content"></section>
 			<p className="CharaName">HERO</p>
 			<section className="dialogue">
-				<DialogueBox />
+				<DialogueBox text={arrayOfDialogue[index]} />
 			</section>
 		</main>
 	);
