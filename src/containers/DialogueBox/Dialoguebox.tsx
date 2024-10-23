db6480e59d073aaa11a91de71b3850fb74bc4ac1
Magaly Fernandez Garcia <fernandezgarciamagaly@gmail.com>
2024-10-23 11:26:00 +0200

    finishing dialogue box

--- src/containers/DialogueBox/Dialoguebox.tsx
+++ src/containers/DialogueBox/Dialoguebox.tsx
@@ -2,11 +2,19 @@ import "./dialoguebox.css";
 
 const DialogueBox = () => {
 	return (
-		<p className="dialogueBox">
-			Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet quod dolor,
-			aperiam tenetur nulla deleniti consectetur nam eaque veniam deserunt, non
-			quia quaerat odit officiis voluptatem fuga quidem rem minima?
-		</p>
+		<>
+			<p className="dialogueBox">
+				Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet quod
+				dolor, aperiam tenetur nulla deleniti consectetur nam eaque veniam
+				deserunt, non quia quaerat odit officiis voluptatem fuga quidem rem
+				minima?
+			</p>
+			<img
+				className="downArrow"
+				src="../../../public/down-long-solid (1).svg"
+				alt="down arrow"
+			/>
+		</>
 	);
 };
 
--- src/containers/PagesCanvas/PagesCanvas.tsx
+++ src/containers/PagesCanvas/PagesCanvas.tsx
@@ -5,6 +5,7 @@ const PageCanvas = () => {
 	return (
 		<main className="main">
 			<section className="content"></section>
+			<p className="CharaName">HERO</p>
 			<section className="dialogue">
 				<DialogueBox />
 			</section>
