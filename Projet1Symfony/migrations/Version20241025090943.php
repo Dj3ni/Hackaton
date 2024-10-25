c5484757574acb54609c37cbf6766b2d96964253
Charifa Znifech <charifaznifech01@gmail.com>
2024-10-25 12:09:51 +0200

    regler le probleme de entity Dialogs

--- Projet1Symfony/migrations/Version20241025090943.php
+++ Projet1Symfony/migrations/Version20241025090943.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20241024200536 extends AbstractMigration
+final class Version20241025090943 extends AbstractMigration
 {
     public function getDescription(): string
     {
--- Projet1Symfony/src/DataFixtures/AssetsFixtures.php
+++ Projet1Symfony/src/DataFixtures/AssetsFixtures.php
@@ -85,6 +85,144 @@ class AssetsFixtures extends Fixture implements DependentFixtureInterface
         $this->addReference("assetCredits",$assetCredits);
         $manager->persist($assetCredits);
 
+        /////assistant avatars \\\\\\\\\\\\\\
+        $assetAvatarAssistantNormal = new Asset();
+        $assetAvatarAssistantNormal 
+                ->setType("avatar")
+                ->setFilepath("/avatars/assistant_normal.png")
+                ->setDescription("assistant normal");
+        $this->addReference("assistant_normal",$assetAvatarAssistantNormal);
+        $manager->persist($assetAvatarAssistantNormal);
+
+        $assetAvatarAssistantHappy = new Asset();
+        $assetAvatarAssistantHappy 
+                ->setType("avatar")
+                ->setFilepath("/avatars/assistant_happy.png")
+                ->setDescription("assistant happy");
+        $this->addReference("assistant_happy",$assetAvatarAssistantHappy);
+        $manager->persist($assetAvatarAssistantHappy);
+
+        $assetAvatarAssistantAngry = new Asset();
+        $assetAvatarAssistantAngry
+                ->setType("avatar")
+                ->setFilepath("/avatars/assistant_angry.png")
+                ->setDescription("assistant angry");
+        $this->addReference("assistant_angry",$assetAvatarAssistantAngry);
+        $manager->persist($assetAvatarAssistantAngry);
+
+
+        ////////parsnip avatars \\\\\\\\\\\\\\
+        $assetAvatarParsnipNormal = new Asset();
+        $assetAvatarParsnipNormal 
+                ->setType("avatar")
+                ->setFilepath("/avatars/parsnip_normal.png")
+                ->setDescription("parsnip normal");
+               
+        $this->addReference("parsnip_normal",$assetAvatarParsnipNormal);
+        $manager->persist($assetAvatarParsnipNormal);
+
+        $assetAvatarParsnipHappy = new Asset();
+        $assetAvatarParsnipHappy 
+                ->setType("avatar")
+                ->setFilepath("/avatars/parsnip_happy.png")
+                ->setDescription("parsnip happy");
+        $this->addReference("parsnip_happy",$assetAvatarParsnipHappy);
+        $manager->persist($assetAvatarParsnipHappy);
+
+        $assetAvatarParsnipAngry = new Asset();
+        $assetAvatarParsnipAngry
+                ->setType("avatar")
+                ->setFilepath("/avatars/parsnip_angry.png")
+                ->setDescription("parsnip angry");
+        $this->addReference("parsnip_angry",$assetAvatarParsnipAngry);
+        $manager->persist($assetAvatarParsnipAngry);
+
+        /////// verdatre avatars \\\\\\\\\\\\\\
+        $assetAvatarVerdatreNormal = new Asset();
+        $assetAvatarVerdatreNormal 
+                ->setType("avatar")
+                ->setFilepath("/avatars/verdatre_normal.png")
+                ->setDescription("verdatre normal");
+        $this->addReference("verdatre_normal",$assetAvatarVerdatreNormal);
+        $manager->persist($assetAvatarVerdatreNormal);
+
+        $assetAvatarVerdatreHappy = new Asset();        
+        $assetAvatarVerdatreHappy
+                ->setType("avatar")     
+                ->setFilepath("/avatars/verdatre_happy.png")
+                ->setDescription("verdatre happy");
+        $this->addReference("verdatre_happy",$assetAvatarVerdatreHappy);
+        $manager->persist($assetAvatarVerdatreHappy);
+
+        $assetAvatarVerdatreAngry = new Asset();
+        $assetAvatarVerdatreAngry
+                ->setType("avatar")
+                ->setFilepath("/avatars/verdatre_angry.png")
+                ->setDescription("verdatre angry");
+        $this->addReference("verdatre_angry",$assetAvatarVerdatreAngry);
+        $manager->persist($assetAvatarVerdatreAngry);
+
+
+
+        ///////////// les backgrounds ////////////////
+
+        $assetBackgroundOffice = new Asset();
+        $assetBackgroundOffice
+                ->setType("background")
+                ->setFilepath("/backgrounds/office-GIF.gif")
+                ->setDescription("office background");
+        $this->addReference("backgroundOffice",$assetBackgroundOffice);
+        $manager->persist($assetBackgroundOffice);
+
+        $assetBackgroundBurningCity = new Asset();
+        $assetBackgroundBurningCity
+                ->setType("background")
+                ->setFilepath("/backgrounds/burning-city-GIF.gif")
+                ->setDescription("burning city background");
+        $this->addReference("backgroundBurningCity",$assetBackgroundBurningCity);
+        $manager->persist($assetBackgroundBurningCity);
+      
+
+        ////////////// les images ////////////
+
+        $assetImageEggBad = new Asset();
+        $assetImageEggBad
+                ->setType("image")
+                ->setFilepath("/images/egg-goes-bad.gif")
+                ->setDescription("egg goes bad");
+        $this->addReference("imageEggBad",$assetImageEggBad);
+        $manager->persist($assetImageEggBad);
+
+        $assetImageEggGood = new Asset();
+        $assetImageEggGood
+                ->setType("image")
+                ->setFilepath("/images/egg-goes-good.gif")
+                ->setDescription("egg goes good");
+        $this->addReference("imageEggGood",$assetImageEggGood);
+
+        $assetImageHeroLogo = new Asset();
+        $assetImageHeroLogo
+                ->setType("image")
+                ->setFilepath("/images/GreenHiro_logo.png")
+                ->setDescription("hero logo");
+        $this->addReference("imageHeroLogo",$assetImageHeroLogo);
+
+        $assetImageStartBtn = new Asset();
+        $assetImageStartBtn
+                ->setType("image")
+                ->setFilepath("/images/start_button.png")
+                ->setDescription("start button");
+        $this->addReference("imageStartBtn",$assetImageStartBtn);
+        $manager->persist($assetImageStartBtn);
+
+
+
+
+
+
+
+
+
 
         // Flush final après avoir mis à jour les relations
         $manager->flush();
@@ -94,7 +232,10 @@ class AssetsFixtures extends Fixture implements DependentFixtureInterface
     {
         return([
             CharacterFixtures::class,
+
         ]);
     }
+
+    
 }
 
